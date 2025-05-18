ec2d892e84c831861a9e6993e7c042f6d5ef28b8
Sangat Rokaya <rokayasangat123@gmail.com>
2025-05-18 12:02:46 +0545

    created auth controller

---
+++ studysync-server/controllers/authController.js
@@ -0,0 +1,57 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// User register
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const exisitngUser = await User.findOne({ email });
+    if (exisitngUser)
+      return res.status(400).json({ message: "User already exists." });
+
+    const hashedPassowrd = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassowrd,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+// User login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "user not found!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid Credentials!" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed!" });
+  }
+};
