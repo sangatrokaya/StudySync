d3ae98abc86657c354dd8cfbb1b04c06f9ccc645
Sangat Rokaya <rokayasangat123@gmail.com>
2025-05-18 12:14:01 +0545

    created auth routes and tested

--- studysync-server/app.js
+++ studysync-server/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Basic health check route
 app.get("/", (req, res) => {
