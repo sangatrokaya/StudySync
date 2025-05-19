501bc6e3a6fc99bb20c71a723aa73fdb31f04521
Sangat Rokaya <rokayasangat123@gmail.com>
2025-05-19 20:32:16 +0545

    privateRoute added for dashboard

--- studysync-client/src/App.jsx
+++ studysync-client/src/App.jsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/Loginpage";
 import MainLayout from "./layouts/MainLayout";
@@ -6,15 +8,38 @@ import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  // Check token once at the top for conditional redirect
+  const token = localStorage.getItem("token");
+
   return (
     <>
       <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          {/* <Route index element={<HomePage />} /> */}
+          <Route index element={<HomePage />} />
+          {/* Redirect to dashboard or login based on token */}
+          <Route
+            index
+            element={
+              token ? (
+                <Navigate to="/dashboard" replace />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
         </Route>
       </Routes>
     </>
